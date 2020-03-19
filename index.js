103a505a4fadd78d8cdb4307b8f4a1c8a104759f
Chris <skullz188@gmail.com>
2020-03-19 09:41:52 +0100

    Inital Commit

---
+++ index.js
@@ -0,0 +1,70 @@
+let drumKey = document.querySelectorAll(".drum").length;
+
+for (let i = 0; i < drumKey; i++) {
+document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+
+
+let buttonInnerHTML = this.innerHTML;
+keySound(buttonInnerHTML);
+
+buttonAnimation(buttonInnerHTML);
+
+
+
+});
+
+}
+
+document.addEventListener("keydown", function(event){
+   keySound(event.key);
+
+   buttonAnimation(event.key);
+
+});
+
+function keySound(key) {
+  switch (key) {
+    case "w":
+    let crash = new Audio("sounds/crash.mp3");
+    crash.play();
+
+      break;
+      case "a":
+      let kick = new Audio("sounds/kick-bass.mp3");
+      kick.play();
+      break;
+      case "s":
+      let snare = new Audio("sounds/snare.mp3");
+      snare.play();
+      break;
+      case "d":
+      let tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+      case "j":
+      let tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+      case "k":
+      let tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      break;
+      case "l":
+      let tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+      break;
+
+    default: console.log(innerHTML);
+
+  }
+}
+
+function buttonAnimation(currentKey) {
+
+  let activeButton = document.querySelector("." + currentKey);
+  activeButton.classList.add("pressed");
+  setTimeout(function(){
+    activeButton.classList.remove("pressed");
+  },100);
+
+}
